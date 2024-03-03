f5781530e5dcb5706143bfea2aa11253e3056d35
JonasKarlsen <jonas.karls@outlokk.com>
2024-03-03 23:43:51 +0100

    automatic stairs funny

--- auto_refuel.lua
+++ auto_refuel.lua
@@ -3,6 +3,10 @@ print("How long you travellin', fella?")
 
 local length = read()
 
+print("Want me to pick up items for ya, chap? (y/n)")
+
+local pickUp = read()
+
 for i=1, length do
     if turtle.getFuelLevel() < 1 then
         for i=1, 16 do
@@ -13,10 +17,38 @@ for i=1, length do
                 print("Refilling fuel")
                 break
             elseif turtle.getSelectedSlot() == 16 then
-                print("No more fuel available. Exiting program.")
+                print("No more fuel available. Thank you for using our service. Sex(c)")
             end
         end
     end
 
-    ## INSERT LE FUNNY CODE HERE
+    if pickUp == "y" then
+        turtle.dig()
+        turtle.suck()
+        turtle.forward()
+        turtle.digUp()
+        turtle.suckUp()
+        turtle.dig()
+        turtle.suck()
+        turtle.digUp()
+        turtle.suckUp()
+        turtle.digDown()
+        turtle.suckDown()
+        turtle.down()
+    elseif pickUp == "n" then
+        turtle.dig()
+        turtle.forward()
+        turtle.digUp()
+        turtle.dig()
+        turtle.digUp()
+        turtle.digDown()
+        turtle.down()
+    else
+        print("WRITE 'y' OR 'n', YOU IIIIDIOT!!")
+    end
+
+    if i=length then
+        print("I'm done, chap! Thank you for using our service!")
+    end
 end
+
